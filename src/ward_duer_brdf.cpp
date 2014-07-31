5ce3a95f5952180ea356212e7f06f863527ddeb8
gatgui <gatgui@gmail.com>
2014-07-31 23:11:14 +0900

    added more options for specular brdf local frame setup

--- src/ward_duer_brdf.cpp
+++ src/ward_duer_brdf.cpp
@@ -6,14 +6,28 @@ enum WardDuerBrdfParams
 {
    p_roughness_x = 0,
    p_roughness_y,
-   p_frame
+   p_local_frame,
+   p_custom_frame
 };
+
+enum LocalFrame
+{
+   LF_UV,
+   LF_polar,
+   LF_shirley,
+   LF_custom
+};
+
+static const char* LocalFrameNames[] = { "UV", "polar", "shirley", "custom", NULL };
 
 node_parameters
 {
    AiParameterFlt("roughness_x", 0.467f);
    AiParameterFlt("roughness_y", 0.467f);
-   AiParameterMtx("frame", AI_M4_IDENTITY);
+   AiParameterEnum("local_frame", LF_UV, LocalFrameNames);
+   AiParameterMtx("custom_frame", AI_M4_IDENTITY);
+   
+   AiMetaDataSetBool(mds, "local_frame", "linkable", false);
 }
 
 node_initialize
@@ -22,6 +36,7 @@ node_initialize
 
 node_update
 {
+   AiNodeSetLocalData(node, (void*) AiNodeGetInt(node, "local_frame"));
 }
 
 node_finish
@@ -30,21 +45,47 @@ node_finish
 
 shader_evaluate
 {
-   BRDFData *brdf_data = (BRDFData*) AiShaderGlobalsQuickAlloc(sg, sizeof(BRDFData));
+   void *local_frame = AiNodeGetLocalData(node);
    
    float rx = AiShaderEvalParamFlt(p_roughness_x);
    float ry = AiShaderEvalParamFlt(p_roughness_y);
-   AtMatrix *frame = AiShaderEvalParamMtx(p_frame);
-   
    AtVector U, V;
    
-   U.x = (*frame)[0][0];
-   U.y = (*frame)[0][1];
-   U.z = (*frame)[0][2];
+   if (local_frame == (void*)LF_UV)
+   {
+      if (!AiV3IsZero(sg->dPdu))
+      {
+         U = AiV3Normalize(sg->dPdu);
+         V = AiV3Normalize(AiV3Cross(sg->Nf, U));
+         U = AiV3Cross(V, sg->Nf);
+      }
+      else
+      {
+         AiBuildLocalFramePolar(&U, &V, &(sg->Nf));
+      }
+   }
+   else if (local_frame == (void*)LF_polar)
+   {
+      AiBuildLocalFramePolar(&U, &V, &(sg->Nf));
+   }
+   else if (local_frame == (void*)LF_shirley)
+   {
+      AiBuildLocalFrameShirley(&U, &V, &(sg->Nf));
+   }
+   else
+   {
+      AtMatrix *frame = AiShaderEvalParamMtx(p_custom_frame);
+      
+      U.x = (*frame)[0][0];
+      U.y = (*frame)[0][1];
+      U.z = (*frame)[0][2];
+      
+      V.x = (*frame)[1][0];
+      V.y = (*frame)[1][1];
+      V.z = (*frame)[1][2];
+   }
    
-   V.x = (*frame)[1][0];
-   V.y = (*frame)[1][1];
-   V.z = (*frame)[1][2];
+   BRDFData *brdf_data = (BRDFData*) AiShaderGlobalsQuickAlloc(sg, sizeof(BRDFData));
    
    brdf_data->evalSample = AiWardDuerMISSample;
    brdf_data->evalBrdf = AiWardDuerMISBRDF;
