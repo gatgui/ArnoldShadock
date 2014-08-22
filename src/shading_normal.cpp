f97244efe0fc9253a26fdf109bea40dd4a0779d7
Gaetan Guidet <gatgui@gmail.com>
2014-08-22 17:10:30 +0900

    added new options to shading_normal/shading_point shaders (space selector and faceforward toggle for shading_normal)

--- src/shading_normal.cpp
+++ src/shading_normal.cpp
@@ -5,14 +5,24 @@ AI_SHADER_NODE_EXPORT_METHODS(ShadingNormalMtd);
 enum ShadingNormalParams
 {
    p_time = 0,
-   p_mode
+   p_mode,
+   p_faceforward,
+   p_space,
+   p_custom_space
 };
 
 enum ShadingNormalMode
 {
-   SN_default = 0,
-   SN_geometric,
-   SN_smoothed
+   SNM_Default = 0,
+   SNM_Geometric,
+   SNM_Smoothed
+};
+
+enum ShadingNormalSpace
+{
+   SNS_World = 0,
+   SNS_Object,
+   SNS_Custom
 };
 
 static const char* ShadingNormalModeNames[] =
@@ -23,45 +33,139 @@ static const char* ShadingNormalModeNames[] =
    NULL
 };
 
+static const char* ShadingNormalSpaceNames[] =
+{
+   "world",
+   "object",
+   "custom",
+   NULL
+};
+
 node_parameters
 {
+   AtMatrix id;
+   AiM4Identity(id);
+   
    AiParameterFlt("time", 0.0f);
-   AiParameterEnum("mode", SN_default, ShadingNormalModeNames);
+   AiParameterEnum("mode", SNM_Default, ShadingNormalModeNames);
+   AiParameterBool("faceforward", false);
+   AiParameterEnum("space", SNS_World, ShadingNormalSpaceNames);
+   AiParameterMtx("custom_space", id);
    
    AiMetaDataSetBool(mds, "mode", "linkable", false);
+   AiMetaDataSetBool(mds, "faceforward", "linkable", false);
+   AiMetaDataSetBool(mds, "space", "linkable", false);
 }
+
+struct ShadingNormalData
+{
+   bool time_is_linked;
+   float time;
+   ShadingNormalMode mode;
+   bool faceforward;
+   ShadingNormalSpace space;
+   bool custom_space_is_linked;
+   AtMatrix custom_space;
+};
 
 node_initialize
 {
+   AiNodeSetLocalData(node, AiMalloc(sizeof(ShadingNormalData)));
 }
 
 node_update
 {
+   ShadingNormalData *data = (ShadingNormalData*) AiNodeGetLocalData(node);
+   
+   data->time_is_linked = AiNodeIsLinked(node, "time");
+   
+   if (!data->time_is_linked)
+   {
+      data->time = AiNodeGetFlt(node, "time");
+   }
+   
+   data->mode = (ShadingNormalMode) AiNodeGetInt(node, "mode");
+   
+   data->faceforward = AiNodeGetBool(node, "faceforward");
+   
+   data->space = (ShadingNormalSpace) AiNodeGetInt(node, "space");
+   
+   data->custom_space_is_linked = AiNodeIsLinked(node, "custom_space");
+   
+   if (data->space == SNS_Custom && !data->custom_space_is_linked)
+   {
+      // custom space -> world matrix
+      AtMatrix tmp;
+      AiNodeGetMatrix(node, "custom_space", tmp);
+      // world -> custom space matrix
+      AiM4Invert(data->custom_space, tmp);
+   }
 }
 
 node_finish
 {
+   AiFree(AiNodeGetLocalData(node));
 }
 
 shader_evaluate
 {
-   ShadingNormalMode mode = (ShadingNormalMode) AiShaderEvalParamInt(p_mode);
+   ShadingNormalData *data = (ShadingNormalData*) AiNodeGetLocalData(node);
    
-   AtPoint P;
-   AtVector N, Ns, Ng;
+   AtPoint P, N, Ns, Ng, outN;
    
-   AiShaderGlobalsGetPositionAtTime(sg, AiShaderEvalParamFlt(p_time), &P, &N, &Ng, &Ns);
+   AiShaderGlobalsGetPositionAtTime(sg, (data->time_is_linked ? AiShaderEvalParamFlt(p_time) : data->time), &P, &N, &Ng, &Ns);
    
-   switch (mode)
+   switch (data->mode)
    {
-   case SN_geometric:
-      sg->out.VEC = Ng;
+   case SNM_Geometric:
+      outN = Ng;
       break;
-   case SN_smoothed:
-      sg->out.VEC = Ns;
+      
+   case SNM_Smoothed:
+      outN = Ns;
       break;
+      
    default:
-      sg->out.VEC = N;
+      outN = N;
    }
    
+   if (data->faceforward)
+   {
+      AiFaceForward(&outN, sg->Rd);
+   }
+   
+   switch (data->space)
+   {
+   case SNS_Object:
+      sg->out.VEC = AiShaderGlobalsTransformNormal(sg, outN, AI_WORLD_TO_OBJECT);
+      break;
+      
+   case SNS_Custom:
+      {
+         AtVector csN;
+         
+         if (data->custom_space_is_linked)
+         {
+            // custom space -> world matrix 
+            AtMatrix *tmp = AiShaderEvalParamMtx(p_custom_space);
+            // world -> custom space matrix
+            AtMatrix custom_space;
+            AiM4Invert(custom_space, *tmp);
+            
+            AiM4VectorByMatrixMult(&csN, custom_space, &outN);
+         }
+         else
+         {
+            AiM4VectorByMatrixMult(&csN, data->custom_space, &outN);
+         }
+         
+         sg->out.VEC = AiV3Normalize(csN);
+      }
+      break;
+      
+   case SNS_World:
+   default:
+      sg->out.VEC = outN;
+      break;
+   }
 }
--- src/shading_point.cpp
+++ src/shading_point.cpp
@@ -4,27 +4,118 @@ AI_SHADER_NODE_EXPORT_METHODS(ShadingPointMtd);
 
 enum ShadingPointParams
 {
-   p_time = 0
+   p_time = 0,
+   p_space,
+   p_custom_space
+};
+
+enum ShadingPointSpace
+{
+   SPS_World = 0,
+   SPS_Object,
+   SPS_Custom
+};
+
+static const char* ShadingPointSpaceNames[] =
+{
+   "world",
+   "object",
+   "custom",
+   NULL
 };
 
 node_parameters
 {
+   AtMatrix id;
+   AiM4Identity(id);
+   
    AiParameterFlt("time", 0.0f);
+   AiParameterEnum("space", SPS_World, ShadingPointSpaceNames);
+   AiParameterMtx("custom_space", id);
+   
+   AiMetaDataSetBool(mds, "space", "linkable", false);
 }
+
+struct ShadingPointData
+{
+   bool time_is_linked;
+   float time;
+   ShadingPointSpace space;
+   bool custom_space_is_linked;
+   AtMatrix custom_space;
+};
 
 node_initialize
 {
+   AiNodeSetLocalData(node, AiMalloc(sizeof(ShadingPointData)));
 }
 
 node_update
 {
+   ShadingPointData *data = (ShadingPointData*) AiNodeGetLocalData(node);
+   
+   data->time_is_linked = AiNodeIsLinked(node, "time");
+   
+   if (!data->time_is_linked)
+   {
+      data->time = AiNodeGetFlt(node, "time");
+   }
+   
+   data->space = (ShadingPointSpace) AiNodeGetInt(node, "space");
+   
+   data->custom_space_is_linked = AiNodeIsLinked(node, "custom_space");
+   
+   if (data->space == SPS_Custom && !data->custom_space_is_linked)
+   {
+      // custom space -> world matrix
+      AtMatrix tmp;
+      AiNodeGetMatrix(node, "custom_space", tmp);
+      // world -> custom space matrix
+      AiM4Invert(data->custom_space, tmp);
+   }
 }
 
 node_finish
 {
+   AiFree(AiNodeGetLocalData(node));
 }
 
 shader_evaluate
 {
-   AiShaderGlobalsGetPositionAtTime(sg, AiShaderEvalParamFlt(p_time), &(sg->out.PNT), 0, 0, 0);
+   ShadingPointData *data = (ShadingPointData*) AiNodeGetLocalData(node);
+   
+   AtPoint outP;
+   
+   AiShaderGlobalsGetPositionAtTime(sg, (data->time_is_linked ? AiShaderEvalParamFlt(p_time) : data->time), &outP, 0, 0, 0);
+   
+   switch (data->space)
+   {
+   case SPS_Object:
+      sg->out.PNT = AiShaderGlobalsTransformPoint(sg, outP, AI_WORLD_TO_OBJECT);
+      break;
+      
+   case SPS_Custom:
+      {
+         if (data->custom_space_is_linked)
+         {
+            // custom space -> world matrix
+            AtMatrix *tmp = AiShaderEvalParamMtx(p_custom_space);
+            // world -> custom space matrix
+            AtMatrix custom_space;
+            AiM4Invert(custom_space, *tmp);
+            
+            AiM4PointByMatrixMult(&(sg->out.PNT), custom_space, &outP);
+         }
+         else
+         {
+            AiM4PointByMatrixMult(&(sg->out.PNT), data->custom_space, &outP);
+         }
+      }
+      break;
+      
+   case SPS_World:
+   default:
+      sg->out.PNT = outP;
+      break;
+   }
 }
