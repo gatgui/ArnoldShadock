781e6a573335a8b92b2735247b8a9518320ab711
Gaetan Guidet <gatgui@gmail.com>
2016-04-15 08:58:34 +0900

    add missing files from previous commit

---
+++ src/strings.cpp
@@ -0,0 +1,24 @@
+#include "strings.h"
+
+namespace SSTR
+{
+   AtString linkable("linkable");
+   AtString source("source");
+   AtString source_color_space("source_color_space");
+   AtString target("target");
+   AtString target_color_space("target_color_space");
+   AtString transform("transform");
+   
+   AtString temperature("temperature");
+   AtString output_mode("output_mode");
+   AtString tm_key("tm_key");
+   AtString tm_Lavg("tm_Lavg");
+   AtString tm_Lwht("tm_Lwht");
+   AtString ca_transform("ca_transform");
+   AtString ca_max_temperature("ca_max_temperature");
+   AtString ca_white_point("ca_white_point");
+   AtString exposure("exposure");
+   AtString color_space("color_space");
+   AtString fix_nans("fix_nans");
+   AtString nan_color("nan_color");
+}
---
+++ src/strings.h
@@ -0,0 +1,29 @@
+#ifndef __agShadingBlocks_strings_h__
+#define __agShadingBlocks_strings_h__
+
+#include <ai.h>
+
+namespace SSTR
+{
+   extern AtString linkable;
+   extern AtString source;
+   extern AtString source_color_space;
+   extern AtString target;
+   extern AtString target_color_space;
+   extern AtString transform;
+   
+   extern AtString temperature;
+   extern AtString output_mode;
+   extern AtString tm_key;
+   extern AtString tm_Lavg;
+   extern AtString tm_Lwht;
+   extern AtString ca_transform;
+   extern AtString ca_max_temperature;
+   extern AtString ca_white_point;
+   extern AtString exposure;
+   extern AtString color_space;
+   extern AtString fix_nans;
+   extern AtString nan_color;
+}
+
+#endif
